d9f6e89ec2576e96b544bd096bc4e7de707cc493
Shachar Shemesh <shachar@shemesh.biz>
2025-12-08 18:15:51 +0100

    Support GPIO bit reset

--- CompuSar.srcs/saros/include/gpio.h
+++ CompuSar.srcs/saros/include/gpio.h
@@ -5,3 +5,8 @@
 
 uint32_t read_gpio(size_t gpio_num);
 void write_gpio(size_t gpio_num, uint32_t value);
+void set_gpio_bits(size_t gpio_num, uint32_t mask);
+void reset_gpio_bits(size_t gpio_num, uint32_t mask);
+
+static constexpr size_t GPIO0__DDR_RESET        = 0x00000001;
+static constexpr size_t GPIO0__DISPLAY32_RESET  = 0x00000002;
